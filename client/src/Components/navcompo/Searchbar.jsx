3ae04a7c858a5139702b0e463cf9079b0ca15a96
Akanksha <akanksharma0829@gmail.com>
2023-11-23 12:49:42 +0530

    commiting changes to searchbar

--- client/src/Components/navcompo/Searchbar.jsx
+++ client/src/Components/navcompo/Searchbar.jsx
@@ -1,345 +1,23 @@
-// import React, { useEffect, useState } from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { Link ,useNavigate} from 'react-router-dom';
-// import { faSearch } from '@fortawesome/free-solid-svg-icons';
-// import { useSelector } from "react-redux";
-// import axios from 'axios';
-// // import _debounce from 'lodash/debounce';
-// import "./Searchbar.css"
-
-// const Searchbar = () => {
-//   const navi =useNavigate();
-//   const [name, setname] = useState("");
-//   const select = useSelector((state) => state.cart.data);
-//   // console.log( select)
-//   const userid = localStorage.getItem("userid");
-//   const filteredItems = select.filter((item) => item.user_id === userid);
-//   // console.log(filteredItems)
-//   const [data1, setdata] = useState([]);
-//   console.log(data1)
-//   const token = localStorage.getItem("token");
-//   // const email = localStorage.getItem("email");
-  
-  
-//   const handleLinkClick = () => {
-//     // Reset the name state to an empty string when a link is clicked
-//     setname("");
-//     window.scroll(0, 0)
-//   };
-  
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//   };
-  
- 
-  
-  
-  
-//   const handletoken = () => {
-    
-//     localStorage.removeItem("token");
-//     localStorage.removeItem("userid");
-//     localStorage.removeItem("email");
-    
-//     navi("/")
-//   };
-//   const handleChange = (e) => {
-//     setname(e.target.value);
-//   };
-  
-// //   useEffect(() => {
-// //     if (name === "") {
-// //       setdata([]);
-// //       return;
-// //     }
-
-// //     const data = {
-// //       search: name,
-// //     };
-// // // const url="https://fignuscart-ly1x.onrender.com/search"
-// // const url="http://localhost:4000/search"
-
-
-// //     axios
-// //       .post(url, data)
-// //       .then((response) => {
-// //         console.log(response.data.data);
-
-// //         setdata(response.data.data);
-// //       })
-// //       .catch((error) => {
-// //         console.error("Error fetching data:", error);
-// //       });
-// //   }, [name]);
-// useEffect(() => {
-//   console.log("Name:", name);
-//   const fetchData = async () => {
-//     try {
-//       if (name === "") {
-//         setdata([]);
-//         return;
-//       }
-
-//       const response = await axios.post("http://localhost:4000/search", {
-//         search: name,
-//       });
-
-//       console.log("Response data:", response.data.data);
-//       setdata(response.data.data);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   fetchData();
-// }, [name]);
-
-
-//   return (
-//     <div className='parentbox'>
-//       <div className='searchbox'>
-//         <input type="text" placeholder="Search products, brands etc" className='search form-control' aria-label="Search" value={name} onChange={handleChange} />
-//         <div className='serachicon ' onClick={handleSubmit}> <FontAwesomeIcon icon={faSearch} /></div>
-// {/* 
-//         <div className="list">
-//           {data1.length > 0 ? (
-//             <ul>
-//               {data1.map((item, index) => (
-//                 <Link
-//                   to={"/detailpage/" + item.id + item.category}
-//                   onClick={handleLinkClick} // Call the function to reset name state
-//                 >
-               
-//                   <li key={index}>{item.name}</li>
-//                 </Link>
-//               ))}
-//             </ul>
-//           ) : null}
-//         </div> */}
-//         {/* Render the results */}
-// <div className="list" style={{color:"black"}}>
-//   {data1.length > 0 ? (
-//     <ul>
-//       {data1.map((item, index) => (
-//         <Link
-//           to={"/detailpage/" + item.id + item.category}
-//           onClick={handleLinkClick}
-//           key={index}
-//         >
-//           <li>{item.name}</li>
-//         </Link>
-//       ))}
-//     </ul>
-//   ) : (
-//     <p>No results found</p>
-//   )}
-
-// </div>
-
-//         <div className="Sign_In">
-//         {token ? (
-//   // Display components for authenticated users
-//   <>
-//     <Link onClick={handletoken} className='btn'>Logout</Link>
-//   </>
-// ) : (
-//   // Display components for non-authenticated users
-//   <Link to={"/login"} className='btn'>SignIn</Link>
-// )}
-//         </div>
-//       </div>
-
-//     </div>
-
-//   )
-// }
-
-// export default Searchbar
-
-
-// chat gpt code 
-
-
-// import React, { useEffect, useState } from "react";
-// import { Link, NavLink } from "react-router-dom";
-// import Badge from '@mui/material/Badge';
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-// import Avatar from '@mui/material/Avatar';
-// // import "./search.css"
-// import "./Searchbar.css"
-
-// import { useSelector } from "react-redux";
-// import axios from "axios";
-
-
-// const Searchbar = () => {
-//   console.log("Component rendered");
-
-//   const [name, setname] = useState("");
-//   const select = useSelector((state) => state.cart.data);
-//   const userid = localStorage.getItem("userid");
-//   const filteredItems = select.filter((item) => item.user_id === userid);
-//   const [data1, setdata] = useState([]);
-//   const token = localStorage.getItem("token");
-//   const email =localStorage.getItem("email");
- 
-//   // .split(" ")[0].toUpperCase();
-// console.log(email)
-
-
-//   // useEffect(() => {
-//   //   console.log("Name before API call:", name);
-//   //   if (name === "") {
-//   //     setdata([]);
-//   //     return;
-//   //   }
-
-//   //   const data = {
-//   //     search: name,
-//   //   };
-
-//   //   axios
-//   //     .post("http://localhost:4000/search", data)
-//   //     .then((response) => {
-//   //       console.log(response); // log the entire response
-//   //       console.log(response.data.data); // log the specific data property
-//   //       setdata(response.data.data);
-//   //     })
-//   //     .catch((error) => {
-//   //       console.log(error);
-//   //     });
-//   // },[name]);
-//   useEffect(() => {
-//     console.log("Name before API call:", name);
-  
-//     const fetchData = async () => {
-//       try {
-//         if (name === "") {
-//           setdata([]);
-//           return;
-//         }
-  
-//         const response = await axios.post("http://localhost:4000/search", {
-//           search: name,
-//         });
-  
-//         console.log("Full Response:", response);
-//         console.log("Response Data:", response.data);
-//         setdata(response.data.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-  
-//     fetchData();
-//   }, [name]);
-  
-//   const handleChange = (e) => {
-//     setname(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//   };
-
-//   const handleLinkClick = () => {
-//     // Reset the name state to an empty string when a link is clicked
-//     setname("");
-//     window.scroll(0,0)
-//   };
-
-//   const handletoken = () => {
-//     localStorage.removeItem("token");
-//     localStorage.removeItem("userid");
-//     localStorage.removeItem("email");
-//   };
-
-//   return (
-//     <div className="searchbar">
-//       <nav className="navbar navbar-light">
-
-//         <div className="container-fluid " >
-//             {/* Logo  */}
-//           <NavLink className="navbar-brand" to={"/"} ><h2><span>E</span>Shop</h2></NavLink>
-
-//           {/* Search Button  */}
-
-//           <div className="cart_and_Sign_In">
-//             <div className="Sign_In">
-//               {
-//                 token ? (<Link onClick={handletoken}>Logout</Link>) : (<Link to={"/login"}>SignIn</Link>)
-//               }
-//               </div>
-            
-//             <div className="avtar">
-//               {
-//                 email ?( <Avatar className="avtar " style={{background:"skyblue"}}>{email.split("")[0].toUpperCase()}</Avatar>) : (<Avatar className="avtar "/>)
-//               }
-//             </div>
-            
-//             <div className="cart_icon">
-//               <Link to={"/order"}>
-//                 {
-//                   filteredItems.length ? (<Badge badgeContent={filteredItems.length} color="primary">
-//                                             <ShoppingCartIcon id="icon" />
-//                                           </Badge>)
-//                                           :
-//                                           (<Badge badgeContent={0} color="primary">
-//                                             <ShoppingCartIcon id="icon" />
-//                                           </Badge>)
-//                 }
-              
-//               </Link>
-              
-
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* Search Form  */}
-//           <form className="Search">
-
-//             <div className="input-group" style={{ height: "17px" }}>
-//               <input className="form-control" type="search" placeholder="Search" aria-label="Search" value={name}onChange={handleChange}/>
-//               <button className="btn btn-outline-success" type="submit" onClick={handleSubmit}>
-//                 <i className="fa-solid fa-magnifying-glass"></i>
-//               </button> 
-//             </div>
-
-//             <div className="list" style={{color:"black" , backgroundColor:"yellow"}}>
-
-//             {data1.length > 0 ? (
-//                 <ul>
-//                   {data1.map((item, index) => (
-//                     <Link
-//                       to={"/product/" + item.id + "/" + item.category}
-//                       onClick={handleLinkClick} // Call the function to reset name state
-//                     >
-//                       <li key={index}>{item.heading}</li>
-//                     </Link>
-//                   ))}
-//                 </ul>
-//             ) : null}
-//             data
-//             </div>
-
-//         </form>
-//       </nav>
-
-
-      
-//     </div>
-//   );
-// };
-
-// export default Searchbar;
-
 // Searchbar.jsx
 
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link ,useNavigate} from 'react-router-dom';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from "react-redux";
 import axios from "axios";
+import "./Searchbar.css"
+
 
 const Searchbar = () => {
+  const navi =useNavigate();
+    const select = useSelector((state) => state.cart.data);
+  console.log( select)
+  const userid = localStorage.getItem("userid");
+  const filteredItems = select.filter((item) => item.user_id === userid);
+  console.log(filteredItems)
+  const token = localStorage.getItem("token");
+
   const [name, setname] = useState("");
   const [data1, setdata] = useState([]);
 
@@ -354,7 +32,7 @@ const Searchbar = () => {
         const response = await axios.post("http://localhost:4000/search", {
           search: name,
         });
-        //  console.log(response.data.data) 
+         console.log(response.data.data) 
         setdata(response.data.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -372,30 +50,72 @@ const Searchbar = () => {
     e.preventDefault();
   };
 
+  const handleLinkClick = () => {
+    // Reset the name state to an empty string when a link is clicked
+    setname("");
+      // Reset the data1 state to an empty array when a link is clicked
+  setdata([]);
+    window.scroll(0, 0)
+  };
+    const handletoken = () => {
+    
+    localStorage.removeItem("token");
+    localStorage.removeItem("userid");
+    localStorage.removeItem("email");
+    
+    navi("/")
+  };
+
+
+
   return (
-    <div>
+    <div className='parentbox'>       
+      <div className='searchbox'>
+
       <form>
         <input
           type="text"
           placeholder="Search products, brands, etc."
           value={name}
+          className='search form-control'
+          aria-label="Search"
           onChange={handleChange}
         />
         <button type="submit" onClick={handleSubmit}>
-          Search
+        <FontAwesomeIcon icon={faSearch} />
         </button>
       </form>
 
-      <div style={{color:"black", backgroundColor:"yellow"}}>
+      <div className="list" style={{color:"black", backgroundColor:"yellow"}}>
         {data1.length >0 ? (
           <ul>
             {data1.slice(0, 5).map((item, index) => (
+              <Link
+                        to={"/detailpage/" + item.id }
+                        onClick={handleLinkClick}
+                        key={index}
+                      >
               <li key={index}>{item.heading}</li>
+              </Link>
+              // <li key={index}>{item.heading}</li>
+
             ))}
           </ul>
         ) : (
-          <p>No results found</p>
+         null
         )}
+      </div>
+      //         <div className="Sign_In">
+//         {token ? (
+  // Display components for authenticated users
+  <>
+    <Link onClick={handletoken} className='btn'>Logout</Link>
+  </>
+) : (
+  // Display components for non-authenticated users
+  <Link to={"/login"} className='btn'>SignIn</Link>
+)}
+        </div>
       </div>
     </div>
   );
